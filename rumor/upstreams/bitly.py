1d5b94ff53284d5a813918fca493ec73ad1d7bbd
Simon Lindroos <simj91@gmail.com>
2019-07-01 15:49:57 +0200

    Add upstream functions for retrieving bitlinks and clicks

--- rumor/upstreams/bitly.py
+++ rumor/upstreams/bitly.py
@@ -4,10 +4,36 @@ from rumor.exceptions import UpstreamError
 
 
 def create_bitlink(long_url: str, access_token: str):
-    bitly_api_url = "https://api-ssl.bitly.com/v4/shorten"
-    headers = {'Authorization': access_token}
     body = {'long_url': long_url}
-    response = requests.post(bitly_api_url, json=body, headers=headers)
+    return _call('POST', '/shorten', access_token, body=body)
+
+
+def get_bitlinks(group_guid: str, access_token: str):
+    return _call('GET', f'/groups/{group_guid}/bitlinks', access_token)['links']
+
+
+def get_bitlink_clicks(bitlink_id: str, access_token: str):
+    response = _call('GET', f'/bitlinks/{bitlink_id}/clicks/summary', access_token)
+    return response['total_clicks']
+
+
+def get_groups(access_token: str):
+    return _call('GET', '/groups', access_token)['groups']
+
+
+def get_primary_group_guid(access_token: str):
+    groups = get_groups(access_token)
+    for group in groups:
+        if group.get('role', '') == 'org-admin':
+            return group['guid']
+    raise UpstreamError()
+
+
+def _call(method: str, endpoint: str, access_token: str, body: dict = None):
+    bitly_api_url = "https://api-ssl.bitly.com/v4"
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = requests.request(method.upper(), f'{bitly_api_url}{endpoint}',
+                                json=body, headers=headers)
     if response.status_code != 200:
         raise UpstreamError()
     return response.json()
--- tests/test_upstreams/test_bitly.py
+++ tests/test_upstreams/test_bitly.py
@@ -3,45 +3,110 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 from rumor.exceptions import UpstreamError
-from rumor.upstreams.bitly import create_bitlink
+from rumor.upstreams.bitly import (create_bitlink, get_bitlink_clicks,
+                                   get_bitlinks, get_groups,
+                                   get_primary_group_guid)
 
 
 @patch('rumor.upstreams.bitly.requests')
-def test_create_bitlink_ok(mock_requests):
-    long_url = "https://example.com?with=some&query=param"
-    access_token = "some-access-token"
-    mock_bitlink = {
-            "id": "bit.ly/abc",
-            "link": "http://bit.ly/abc",
-            "long_url": long_url
-            }
+def test_create_bitlink_ok(mock_requests, bitly_access_token, bitly_bitlink,
+                           bitly_long_url, bitly_api_url):
     mock_response = MagicMock()
     mock_response.status_code = 200
-    mock_response.json.return_value = mock_bitlink
-    mock_requests.post.return_value = mock_response
+    mock_response.json.return_value = bitly_bitlink
+    mock_requests.request.return_value = mock_response
 
-    bitlink = create_bitlink(long_url, access_token)
+    bitlink = create_bitlink(bitly_long_url, bitly_access_token)
 
-    assert bitlink == mock_bitlink
-    mock_requests.post.assert_called_once_with(
-            "https://api-ssl.bitly.com/v4/shorten",
-            json={"long_url": long_url},
-            headers={'Authorization': access_token})
+    assert bitlink == bitly_bitlink
+    mock_requests.request.assert_called_once_with(
+            'POST', f'{bitly_api_url}/shorten',
+            json={"long_url": bitly_long_url},
+            headers={'Authorization': f'Bearer {bitly_access_token}'})
 
 
 @patch('rumor.upstreams.bitly.requests')
-def test_create_bitlink_fail(mock_requests):
-    long_url = "https://example.com?with=some&query=param"
-    access_token = "some-access-token"
-
+def test_create_bitlink_fail(mock_requests, bitly_access_token, bitly_long_url, bitly_api_url):
     mock_response = MagicMock()
     mock_response.status_code = 500
-    mock_requests.post.return_value = mock_response
+    mock_requests.request.return_value = mock_response
 
     with pytest.raises(UpstreamError):
-        create_bitlink(long_url, access_token)
+        create_bitlink(bitly_long_url, bitly_access_token)
 
-    mock_requests.post.assert_called_once_with(
-            "https://api-ssl.bitly.com/v4/shorten",
-            json={"long_url": long_url},
-            headers={'Authorization': access_token})
+    mock_requests.request.assert_called_once_with(
+            'POST', f'{bitly_api_url}/shorten',
+            json={"long_url": bitly_long_url},
+            headers={'Authorization': f'Bearer {bitly_access_token}'})
+
+
+@patch('rumor.upstreams.bitly.requests')
+def test_get_bitlinks_ok(mock_requests, bitly_group_guid, bitly_access_token,
+                         bitly_bitlink, bitly_api_url):
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {'links': [bitly_bitlink]}
+    mock_requests.request.return_value = mock_response
+
+    bitlinks = get_bitlinks(bitly_group_guid, bitly_access_token)
+
+    assert bitlinks == [bitly_bitlink]
+    mock_requests.request.assert_called_once_with(
+            'GET', f'{bitly_api_url}/groups/{bitly_group_guid}/bitlinks',
+            headers={'Authorization': f'Bearer {bitly_access_token}'},
+            json=None)
+
+
+@patch('rumor.upstreams.bitly.requests')
+def test_get_groups_ok(mock_requests, bitly_group, bitly_group_guid,
+                       bitly_access_token, bitly_api_url):
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {'groups': [bitly_group]}
+    mock_requests.request.return_value = mock_response
+
+    groups = get_groups(bitly_access_token)
+
+    assert groups == [bitly_group]
+    mock_requests.request.assert_called_once_with(
+            'GET', f'{bitly_api_url}/groups',
+            headers={'Authorization': f'Bearer {bitly_access_token}'},
+            json=None)
+
+
+@patch('rumor.upstreams.bitly.requests')
+def test_get_primary_group_guid_ok(mock_requests, bitly_group, bitly_group_guid,
+                                   bitly_access_token, bitly_api_url):
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {'groups': [bitly_group]}
+    mock_requests.request.return_value = mock_response
+
+    group_guid = get_primary_group_guid(bitly_access_token)
+
+    assert group_guid == bitly_group_guid
+    mock_requests.request.assert_called_once_with(
+            'GET', f'{bitly_api_url}/groups',
+            headers={'Authorization': f'Bearer {bitly_access_token}'},
+            json=None)
+
+
+@patch('rumor.upstreams.bitly.requests')
+def test_get_bitlink_clicks(mock_requests, bitly_access_token, bitly_bitlink,
+                            bitly_api_url, bitly_bitlink_id):
+
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {
+        "total_clicks": 17,
+    }
+
+    mock_requests.request.return_value = mock_response
+
+    clicks = get_bitlink_clicks(bitly_bitlink_id, bitly_access_token)
+
+    assert clicks == 17
+    mock_requests.request.assert_called_once_with(
+            'GET', f'{bitly_api_url}/bitlinks/{bitly_bitlink_id}/clicks/summary',
+            headers={'Authorization': f'Bearer {bitly_access_token}'},
+            json=None)
